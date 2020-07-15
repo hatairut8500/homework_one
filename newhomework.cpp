9ef87d5824823d5ec4e580a169415270a7b49872
hatairut8500 <68267188+hatairut8500@users.noreply.github.com>
2020-07-15 14:59:27 +0700

    Add files via upload

---
+++ newhomework.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+
+void main()
+{
+	  string name ;
+	  int   old  , age  ;
+	  cout<< " enter name : " ;  cin>> name ;  cout<< name ;
+	  cout<< "enter birth : "  ;  cin >> old  ; cout<< old  << endl ;
+         age = 2563 - old ;
+
+		 cout << age << endl ;
+}         
